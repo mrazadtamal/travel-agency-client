e37c7a75e01fe8129a1560ddbaf65252eb3bb3fd
mrazadtamal <mrazadt@gmail.com>
2022-01-25 20:21:49 +0600

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>azad</h1>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 
