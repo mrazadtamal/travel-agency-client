6cab252c668ad4abcd3c38929dbb7d0a047b6599
mrazadtamal <mrazadt@gmail.com>
2022-01-26 23:27:55 +0600

    blog

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
         <BrowserRouter>
           <Navbar />
           <Routes>
+            <Route path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
